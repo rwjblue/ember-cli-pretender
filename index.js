eac7afe2648fcac31ac7ed17f00dd7ff025eb92d
Robert Jackson <robert.w.jackson@me.com>
2014-06-13 16:05:09 -0400

    Cleanup and make work properly.

--- index.js
+++ index.js
@@ -5,24 +5,30 @@ var fs   = require('fs');
 
 function EmberCLIPretender(project) {
   this.project = project;
-  this.name = 'Ember Random Addon';
+  this.name    = 'Ember CLI Pretender';
 }
 
-function unwatchedTree(dir){
-  return {read: function() { return dir; }};
+function unwatchedTree(dir) {
+  return {
+    read:    function() { return dir; },
+    cleanup: function() { }
+  };
 }
 
 EmberCLIPretender.prototype.treeFor = function treeFor(name) {
-  var treePath = path.join(__dirname, name);
+  var treePath =  path.join('node_modules/ember-cli-pretender', name);
 
-  if (fs.existsSync(treePath)) {
+  if (this.app.env !== 'production' && fs.existsSync(treePath)) {
     return unwatchedTree(treePath);
   }
 };
 
 EmberCLIPretender.prototype.included = function included(app) {
   this.app = app;
-  this.app.import('vendor/pretender/pretender.js');
+
+  if (this.app.env !== 'production') {
+    this.app.import('vendor/pretender/pretender.js');
+  }
 };
 
 module.exports = EmberCLIPretender;
