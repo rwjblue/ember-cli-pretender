0de7af774235968cbd12afe29acc533b36d632e5
Robert Jackson <robert.w.jackson@me.com>
2014-08-25 07:08:00 -0400

    Use default Addon implementation from ember-cli.

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 'use strict';
 
-var path = require('path');
-var fs   = require('fs');
+module.exports = {
+  name: 'ember-cli-pretender',
 
-function EmberCLIPretender(project) {
-  this.project = project;
-  this.name    = 'Ember CLI Pretender';
-}
+  included: function included(app) {
+    this.app = app;
 
-function unwatchedTree(dir) {
-  return {
-    read:    function() { return dir; },
-    cleanup: function() { }
-  };
-}
-
-EmberCLIPretender.prototype.treeFor = function treeFor(name) {
-  var treePath =  path.join('node_modules/ember-cli-pretender', name + '-addon');
-
-  if (this.app.env !== 'production' && fs.existsSync(treePath)) {
-    return unwatchedTree(treePath);
+    if (this.app.env !== 'production') {
+      this.app.import('vendor/FakeXMLHttpRequest/fake_xml_http_request.js');
+      this.app.import('vendor/route-recognizer/dist/route-recognizer.js');
+      this.app.import('vendor/pretender/pretender.js');
+    }
   }
 };
-
-EmberCLIPretender.prototype.included = function included(app) {
-  this.app = app;
-
-  if (this.app.env !== 'production') {
-    this.app.import('vendor/FakeXMLHttpRequest/fake_xml_http_request.js');
-    this.app.import('vendor/route-recognizer/dist/route-recognizer.js');
-    this.app.import('vendor/pretender/pretender.js');
-  }
-};
-
-module.exports = EmberCLIPretender;
