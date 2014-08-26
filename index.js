fd4d7d95c09b2456b4c71b8b7686282630192648
Robert Jackson <robert.w.jackson@me.com>
2014-08-26 16:13:42 -0400

    Add shim file to make pretender importable.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = {
       this.app.import('vendor/FakeXMLHttpRequest/fake_xml_http_request.js');
       this.app.import('vendor/route-recognizer/dist/route-recognizer.js');
       this.app.import('vendor/pretender/pretender.js');
+      this.app.import('vendor/ember-cli-pretender/shim.js');
     }
   }
 };
