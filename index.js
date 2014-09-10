1a432880f3c46fa13620927937a64ea724619fab
Stefan Penner <stefan.penner@gmail.com>
2014-09-10 09:32:49 -0700

    ensure correctly vendor directory is used

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ module.exports = {
   included: function included(app) {
     this.app = app;
 
-    if (this.app.env !== 'production') {
-      this.app.import('vendor/FakeXMLHttpRequest/fake_xml_http_request.js');
-      this.app.import('vendor/route-recognizer/dist/route-recognizer.js');
-      this.app.import('vendor/pretender/pretender.js');
-      this.app.import('vendor/ember-cli-pretender/shim.js', {
+    if (app.env !== 'production') {
+      app.import(app.bowerDirectory + '/FakeXMLHttpRequest/fake_xml_http_request.js');
+      app.import(app.bowerDirectory + '/route-recognizer/dist/route-recognizer.js');
+      app.import(app.bowerDirectory + '/pretender/pretender.js');
+      app.import('vendor/ember-cli-pretender/shim.js', {
         type: 'vendor',
         exports: { 'pretender': ['default'] }
       });
