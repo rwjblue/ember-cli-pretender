5d1b2f99a935d1b87bd58e250b28a1f38b1b32fa
Cory Forsyth <cory.forsyth@gmail.com>
2014-12-11 16:30:42 -0500

    correct pretender version is 0.4.2, not 0.4.3
    The [commit message in the pretender repo says 0.4.3](https://github.com/trek/pretender/commit/0a802e8c544bae1ec8a1987d7586388d677bbd91), but in actuality the version was only bumped to 0.4.2.

--- blueprints/ember-cli-pretender/index.js
+++ blueprints/ember-cli-pretender/index.js
@@ -8,6 +8,6 @@ module.exports = {
   },
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('pretender', '^0.4.3');
+    return this.addBowerPackageToProject('pretender', '^0.4.2');
   }
 };
